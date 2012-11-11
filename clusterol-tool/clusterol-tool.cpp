2e299d55de22019def3ac6b1eccebdd6596f9ceb
Karsten Looschen <karsten.looschen@gmail.com>
2012-11-11 23:12:45 +0100

    Single-link clustering seems to work.

--- clusterol-tool/clusterol-tool.cpp
+++ clusterol-tool/clusterol-tool.cpp
@@ -1,10 +1,14 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <stdexcept>
-#include <string>
-#include <vector>
 #include <boost/program_options.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/version.hpp>
+#if BOOST_VERSION >= 104100
+#include <boost/property_map/property_map.hpp>
+#else
+#include <boost/property_map.hpp>
+#endif
 
 #include "clusterol/dissimilarity.hpp"
 #include "clusterol/lance_williams.hpp"
@@ -68,14 +72,31 @@ int main(int argc, char *argv[]){
     exit(1);
   }
 
-  // test output
-  for(std::vector<data_point>::iterator i = data_set.begin(); i != data_set.end(); ++i){
-    for(data_point::iterator j = i->begin(); j != i->end(); ++j)
-      std::cout << *j << " ";
-    std::cout << "\n";
-  }
-  
+  // // test output
+  // for(std::vector<data_point>::iterator i = data_set.begin(); i != data_set.end(); ++i){
+  //   for(data_point::iterator j = i->begin(); j != i->end(); ++j)
+  //     std::cout << *j << " ";
+  //   std::cout << "\n";
+  // }
 
+
+  // cluster, preliminary testing implementation
+  clusterol::lance_williams_generic lw(0.5, 0.5, 0, -0.5); // single link 
+  // lance_williams lw(0.5, 0.5, 0, 0.5);	// complete link
+  // lance_williams lw(0.5, 0.5, 0, 0);	// weighted group average
+  // lance_williams lw(0.5, 0.5, -0.25, 0);// Median (weighted centroid
+
+  boost::adjacency_list<> dendrogram;
+  boost::graph_traits< boost::adjacency_list<> >::vertex_descriptor root;
+  std::vector<double> height(2*data_set.size() - 1);
+  clusterol::matrix_clustering
+    (data_set.begin(), data_set.end(),
+     dendrogram, root, &height[0],
+     clusterol::dissimilarity_be<clusterol::euclidean_distance>(),
+     lw);
+
+  boost::write_graphviz(std::cout, dendrogram, boost::make_label_writer(&height[0]));
+  
 
   return 0;
 }
