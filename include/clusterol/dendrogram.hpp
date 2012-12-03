0b273c79e44a0c8c54a06b082a3b8ddad59f71bd
Karsten Looschen <karsten.looschen@gmail.com>
2012-12-03 10:54:02 +0100

    Fix dendrogram default initialization
    Vector sizes should not be negative.

--- include/clusterol/dendrogram.hpp
+++ include/clusterol/dendrogram.hpp
@@ -15,8 +15,8 @@ namespace clusterol{
     typedef boost::graph_traits<tree_type>::vertex_descriptor vertex_descriptor;
     typedef join_report_entry<height_type, vertex_descriptor> join_report_entry_type;
 
-    // convenient constructor, which allows default construction
-    dendrogram(size_t n_data_point = 0)
+    // convenient constructor
+    dendrogram(size_t n_data_point)
       : tree(tree_type(n_data_point)),
 	root(0),
 	size(2 * n_data_point - 1),
@@ -26,7 +26,11 @@ namespace clusterol{
       for(size_t i = 0; i != n_data_point; ++i)
 	size[i] = 1;
     }
+
+    // allow default construction
+    dendrogram() {}
     
+
     tree_type tree;
     // mostly unnecessary, since last vertex in tree is (always?) root:
     vertex_descriptor root;	
